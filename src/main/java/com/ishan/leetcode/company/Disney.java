12cc4a54cd04fec42ec347c003fea6a1b277268f
Ishan Shah <ishanshah@Ishans-MacBook-Pro.local>
2023-03-01 18:17:23 -0800

    parking garage updates

--- src/main/java/com/ishan/leetcode/company/Disney.java
+++ src/main/java/com/ishan/leetcode/company/Disney.java
@@ -8,17 +8,106 @@
 
 package com.ishan.leetcode.company;
 
+import java.util.*;
+
 public class Disney {
 
     static  class Vehicle{
         String id;
+
+        public Vehicle(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+    }
+    class SpotAssigned{
+        Vehicle vehicle;
+        int spotNumber;
+
+        long startTime;
+
     }
 
     class ParkingGarage{
+        int capacity;
+        double costInSeconds=0.0;
+        int vehiclesParked=0;
+        boolean[] isOccupied;
+        Map<Long,Double> hashMap=new TreeMap<>();
+        Map<String,SpotAssigned> spotAssignedMap=new HashMap<>();
+        public ParkingGarage(int capacity,double costInSeconds) {
+            this.capacity=capacity;
+            this.costInSeconds=costInSeconds;
+            isOccupied=new boolean[capacity];
+        }
+
+        // Randomly assign SPOT
+        private boolean park(Vehicle v){
+                    if(capacity-vehiclesParked>=0){
+                        return false;
+                    }
+                    //Spot assign
+
+                int randomNumber = 0;
+                while(isOccupied[randomNumber]!=Boolean.FALSE){
+                    randomNumber=new Random().nextInt(0, capacity);
+                }
+                isOccupied[randomNumber]=Boolean.TRUE;
+                this.vehiclesParked++;
 
 
-        //park -random parking
-        // exit
-        //getEarnings
+                SpotAssigned spotAssigned=new SpotAssigned();
+                spotAssigned.spotNumber=randomNumber;
+                spotAssigned.vehicle=v;
+                spotAssigned.startTime=System.currentTimeMillis();
+
+                return true;
+        }
+
+        //Charge the customer when they exit
+        private void exit(Vehicle v){
+
+            if(spotAssignedMap.get(v.getId())==null){
+                //exit
+                return;
+            }
+
+            SpotAssigned spotAssigned = spotAssignedMap.get(v.getId());
+
+            long exitTime=System.currentTimeMillis();
+
+
+            double cost=this.costInSeconds * (exitTime-spotAssigned.startTime);
+
+            if(hashMap.containsKey(exitTime)){
+                hashMap.put(exitTime, hashMap.get(exitTime) + cost);
+            }else{
+                hashMap.put(exitTime, cost);
+            }
+
+
+            spotAssignedMap.remove(v.getId());
+            this.vehiclesParked--;
+
+        }
+       // 10 thi 11
+        // Inclusive
+        private Double getEarnings(long startTime, long endTime){
+            double totalEarnings=0.0;
+            for(Map.Entry<Long,Double> hs:hashMap.entrySet()){
+
+                if(hs.getKey()>=startTime && hs.getKey()<=endTime){
+                    totalEarnings+=hs.getValue();
+                }
+            }
+
+            return totalEarnings;
+
+        }
+
     }
 }
