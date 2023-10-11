ff2d7702751297e8ae344761e76dc1dcbc5ba66f
Ishan Shah <ishanshah@Ishans-MacBook-Pro.local>
2023-10-10 18:48:11 -0700

    oct 10

--- src/main/java/com/ishan/blind75/Readme.md
+++ src/main/java/com/ishan/blind75/Readme.md
@@ -1,3 +1,14 @@
 https://www.techinterviewhandbook.org/grind75
 
 https://www.techinterviewhandbook.org/grind75?weeks=14&hours=11&grouping=topics&mode=all
+
+
+
+## Oct 10
+
+** https://aaronice.gitbook.io/lintcode/sweep-line/meeting-rooms
+** https://leetcode.com/problems/contains-duplicate/
+** https://leetcode.com/problems/sort-colors/
+** https://leetcode.com/problems/valid-anagram/
+
+## Oct 10
---
+++ src/main/java/com/ishan/blind75/oct10/SortColors.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.ishan.blind75.oct10;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/sort-colors/
+public class SortColors {
+    // doing it for 2 integers first
+    public static void main(String[] args) {
+       //sortTwoColors();
+
+       sortThreeColors();
+    }
+
+    private static void sortThreeColors() {
+        int[]  nums = {0,1,1,0,2,2,1,1,0,2,1};
+
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        for (int i = 0; i <= r;)
+            if (nums[i] == 0)
+                swap(nums, i++, l++);
+            else if (nums[i] == 1)
+                i++;
+            else
+                swap(nums, i, r--);
+        System.out.println(Arrays.toString(nums));
+
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void sortTwoColors() {
+        int[]  nums = {0,1,1,0};
+
+        int j=nums.length -1;
+        int i=0;
+        for(int k=0;k<nums.length;k++){
+            if(i!=j){
+                if(nums[i]==1 && nums[j]==0){
+                    nums[i]=0;
+                    nums[j]=1;
+                    i++;
+                    j--;
+                }else if (nums[i]==0 ){
+                    i++;
+                }else if(nums[j]==1){
+                    j--;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
