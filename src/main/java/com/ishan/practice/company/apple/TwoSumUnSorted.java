583febaa347d75c1b988d068dd2eec7147268ad3
ishanshah <ishanshah301190@gmail.com>
2025-02-19 22:44:11 -0800

    Oct22

---
+++ src/main/java/com/ishan/practice/company/apple/TwoSumUnSorted.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.ishan.practice.company.apple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumUnSorted {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
+        public static void main(String[] args) {
+            int[] numbers = new int[]{12, 7, 2, 15};
+            int target = 9;
+            int[] result = twoSum(numbers, target);
+            System.out.println(result[0] + " " + result[1]);
+        }
+}
